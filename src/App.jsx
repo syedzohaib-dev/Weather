f089128429218a5d76c8a8dba7e23c98d0038d7a
syed zohaib <zohaibsyed914@gmail.com>
2024-11-26 17:29:06 +0500

    working

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./App.css"
+import Mobile from './Components/Mobile'
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <Mobile />
+      </div>
+    </>
+
+  )
+}
+
+
+export default App
---
+++ src/Components/Mobile.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Mobile.css"
+
+function Mobile() {
+    return (
+        <>
+            <div className="mobileDiv">
+                <div className="topNotch"></div>
+                <video autoplay muted loop  src="/videos/clear.mp4"></video>
+            </div>
+        </>
+    )
+}
+
+export default Mobile
